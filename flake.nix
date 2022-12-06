fabe6788a78ff5d5712b2f7546b56047745f72d8
Philipp Böschen <catouc@philipp.boeschen.me>
2022-12-06 21:29:19 +0100

    Bump version to v0.8.1

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         build = pkgs.buildGoModule {
           pname = "jiwa";
-          version = "v0.8.0";
+          version = "v0.8.1";
           modSha256 = pkgs.lib.fakeSha256;
           vendorSha256 = null;
           src = ./.;
