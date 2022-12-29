5fa4ba706122b6aea8f0669030c24f0141ae56ea
Philipp Böschen <philipp.boeschen@booking.com>
2022-12-29 15:15:22 +0100

    Bump to v0.11.0

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         build = pkgs.buildGoModule {
           pname = "jiwa";
-          version = "v0.10.1";
+          version = "v0.11.0";
           modSha256 = pkgs.lib.fakeSha256;
           vendorSha256 = null;
           src = ./.;
